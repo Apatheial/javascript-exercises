e03423970f3244664f56c01f16408ff69a1173db
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-03-23 20:55:14 -0300

    feat: complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    const indexIsNumber = +index;
+    const fibonacci = [1, 1];
+    if (indexIsNumber < 0) return "OOPS";
+    if (indexIsNumber === 0) return 0;
+    for (let i = 0; i < indexIsNumber; i++) {
+        fibonacci.push(fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2])
+    }
 
+    return fibonacci[indexIsNumber - 1];
 };
 
 // Do not edit below this line
