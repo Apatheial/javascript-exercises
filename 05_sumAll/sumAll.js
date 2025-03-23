fca417a8160def4c0b4705477fe00fe4b2672db7
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-03-23 18:08:05 -0300

    feat: complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
 
+    if (!(Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) 
+        || firstNumber < 0 || secondNumber < 0) 
+        return "ERROR";
+
+    let min = Math.min(firstNumber, secondNumber);
+    let max = Math.max(firstNumber, secondNumber);
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
