b9717048b0200790bec57aac3196a26371a282b7
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-03-23 17:03:06 -0300

    feat: complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
+    let newString = "";
+    
+    if (repeatCount < 0) return "ERROR";
 
+    for (let i = 0; i < repeatCount; i++) {
+        newString += string;
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
