0a21e9603c66a62e88f4cf86af20a613bec8e8f1
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-03-23 19:22:47 -0300

    feat: complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	 return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, num) => accumulator + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((accumulator, num) => accumulator * num, 1);
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+  return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
