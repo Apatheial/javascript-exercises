4dc925ec7cdf116b9a041ce9b7b6e4366298e934
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-03-23 19:35:48 -0300

    feat: complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const temp = string.toLowerCase()
+                        .replace(/[,.!? ]/g, "")
+    const palindrome = temp.toLowerCase()
+                            .split("")
+                            .reverse()
+                            .join("");
+    return temp === palindrome ? true : false;
 };
 
 // Do not edit below this line
